362902e49745846bfc18919ce65dc811de02da80
Anton Mudrov <mudrov.ant@yandex.ru>
2024-06-08 23:44:35 +0300

    add allure report

---
+++ src/main/java/ru/praktikum/model/constants/Missing.java
@@ -0,0 +1,6 @@
+package praktikum.model.constants;
+
+public class Missing {
+    public static final String MISSING_LOGIN = null;
+    public static final String MISSING_PASSWORD = null;
+}
---
+++ src/main/java/ru/praktikum/steps/CourierSteps.java
@@ -0,0 +1,59 @@
+package praktikum.steps;
+import io.qameta.allure.Step;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import ru.praktikum.dto.CourierCreateRequest;
+import ru.praktikum.dto.CourierLoginRequest;
+import static io.restassured.RestAssured.given;
+import static ru.praktikum.config.RestConfig.HOST;
+import static ru.praktikum.model.constants.ApiLink.*;
+
+public class CourierSteps {
+
+    @Step("Создание курьера")
+    public ValidatableResponse createCourier (String login, String password, String firstName) {
+
+        CourierCreateRequest courierCreateRequest = new CourierCreateRequest ();
+        courierCreateRequest.setLogin (login);
+        courierCreateRequest.setPassword (password);
+        courierCreateRequest.setFirstName (firstName);
+
+        return given()
+                .contentType (ContentType.JSON)
+                .baseUri (HOST)
+                .body(courierCreateRequest)
+                .when()
+                .post (CREATE_COURIER)
+                .then();
+    }
+
+   @Step("Логин курьера в системе")
+    public ValidatableResponse loginCourier (String login, String password) {
+
+        CourierCreateRequest courierCreateRequest = new CourierCreateRequest ();
+        CourierLoginRequest courierLoginRequest = new CourierLoginRequest ();
+
+        courierCreateRequest.setLogin (login);
+        courierCreateRequest.setPassword (password);
+        courierLoginRequest.setLogin (login);
+        courierCreateRequest.setPassword (password);
+
+        return given()
+                .contentType (ContentType.JSON)
+                .baseUri (HOST)
+                .body(courierCreateRequest)
+                .when()
+                .post(LOGIN_COURIER)
+                .then();
+    }
+    @Step("Удаление созданного курьера")
+    public ValidatableResponse delete (int id) {
+        return given()
+                .contentType (ContentType.JSON)
+                .baseUri (HOST)
+                .pathParams ("id", id)
+                .when()
+                .delete (DELETE)
+                .then();
+    }
+}
